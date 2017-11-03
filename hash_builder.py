c2587300d29b18b806de3f6398f9ecd4664df0cb
Patrick Mendus <pmendus@hawk.iit.edu>
2017-11-03 15:38:48 -0500

    Iris recognition runner updated

--- hash_builder.py
+++ hash_builder.py
@@ -1,18 +1,12 @@
 import os
 
-
 def build(location, arr):
     for root, dirs, files in os.walk(location):
         for file in files:
             if not os.path.isdir(file):
                 if  file.endswith('txt'):
-                    # print(os.path.dirname(file))
-                    # print(location)
                     p = root+'\\'+file
-                    # print(p)
                     handle_file(p, arr)
-
-
 
 def handle_file(filepath, arr):
     str=""
@@ -25,39 +19,27 @@ def handle_file(filepath, arr):
             arr.append( create_hash(str, filepath))
             str=""
 
-
-
-
-
-
 def create_hash(arr, filepath):
-    dict = {'iris_code': ''}
+    dict = {'iris_code': None}
 
     noprefix = prefix(filepath)
 
-    # print(os.path.dirname(filepath))
     for line in arr.splitlines():
         if 'subjectid' in line:
             list_of_words = line.split()
             subjectid = list_of_words[list_of_words.index('string') + 1]
             dict['id'] = subjectid
-            # dict['img'] = filepath
         elif noprefix in line:
             list2 = line.split()
             address = list2[list2.index('file')+2]
             filename= purge(address)
-            dict['img'] = build_dir(filepath, filename)
-            # print(address)
-    # print(dict)
+            dict['image'] = build_dir(filepath, filename)
     return dict
 
-
 def prefix(filepath):
-    # print(filepath)
     rgal = filepath.split('\\')[1]
     if 'probe' in rgal:
         rgal = filepath.split('\\')[2]
-    # print(rgal)
     p1 = rgal.split('-')[0]
     p2= rgal.split('-')[1]
     p3 = p1+'/'+p2
@@ -66,7 +48,6 @@ def prefix(filepath):
 def purge(address):
     r6 = address.split('/')
     r7 = r6[len(r6)-1]
-    # print(r7)
     return r7
 
 def build_dir(filepath, filename):
--- iris_recognition.py
+++ iris_recognition.py
@@ -1,25 +1,26 @@
 import cv2
 import numpy as np
 
-THRESHOLD = 0.45
+THRESHOLD = 0.3
 
 def iris_recognition(gallery, probe):
     probe_code = collapse_iris_code_into_array(probe['iris_code'])
     closest_template = {}
     lowest_hamming_distance = 1
+    match = False
     for template in gallery:
+        if template['id'] == probe['id']:
+            match = True
         template_code = collapse_iris_code_into_array(template['iris_code'])
         hamming_distance = compare_codes(template_code, probe_code)
         if lowest_hamming_distance > hamming_distance:
             lowest_hamming_distance = hamming_distance
             closest_template = template
-    match = False
-    if closest_template['id'] == probe['id']:
-        match = True
+    
     accept = False
     if lowest_hamming_distance < THRESHOLD:
         accept = True
-    return { 'match': match, 'accept': accept, 'hd': lowest_hamming_distance }
+    return { 'match': match, 'accept': accept, 'hd': lowest_hamming_distance, 'closest_match_id': closest_template['id'], 'probe_id': probe['id'] }
 
 def collapse_iris_code_into_array(code_image):
     code_array = []
@@ -35,3 +36,10 @@ def compare_codes(template_code, probe_code):
         count += 1
     distance = differences/count
     return distance
+
+def purge_errors(templates):
+    purged_templates = []
+    for template in templates:
+        if template['iris_code'] is not None:
+            purged_templates.append(template)
+    return purged_templates
